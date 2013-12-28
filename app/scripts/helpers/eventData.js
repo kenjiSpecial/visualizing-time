a10390635d7ccc3d433ed8dcce5655e2b3814d70
kenji-special <k.saito.1985@gmail.com>
2013-12-28 17:57:05 -0500

    update.

--- app/scripts/helpers/eventData.js
+++ app/scripts/helpers/eventData.js
@@ -5,75 +5,79 @@ define([
     'helpers/commonData'
 ], function (CommonData) {
 
+    var balkans = ['albania', 'bosnia', 'bulgaria', 'croatia', 'greece', 'kosovo', 'macedonia', 'montenegro', 'romania', 'serbia', 'turkey'];
+    var austria = ['austria', 'hungary', 'czech', 'slovakia'];
+    var russia = ['russia'];
+
     var eventData = {
         // --------------------
     	// 'german unification'
-    	'2ffcd862-2f47-436b-960e-8fc234267ea7' : ['gallery'], // 'Léon Gambetta'
-    	'55ac2ff1-fef1-4006-a749-d118fc1c0ac2' : ['gallery'], // 'Finding Balance'
-    	'c4738c91-57bd-47b2-857a-60bb5764ce3e' : ['gallery'], // Creation of "New Germany"
-    	'31daff9d-91f4-43a9-b6ba-778b7f22b8f5' : ['gallery'], // The Great Illusion
-    	'4184c143-98bd-4ca3-bfca-d40ed34acc9b' : ['map'], // Were Germans Unified?
-    	'17387c82-756d-4571-b611-3380f33eeee4' : ['map'], // What Does It Mean?!
+    	'2ffcd862-2f47-436b-960e-8fc234267ea7' : ['gallery', ['germany', 'france']], // 'Léon Gambetta'
+    	'55ac2ff1-fef1-4006-a749-d118fc1c0ac2' : ['gallery', ['germany', 'france']], // 'Finding Balance'
+    	'c4738c91-57bd-47b2-857a-60bb5764ce3e' : ['gallery', ['germany', 'france']], // Creation of "New Germany"
+    	'31daff9d-91f4-43a9-b6ba-778b7f22b8f5' : ['gallery', ['uk', 'germany', 'france', 'russia']], // The Great Illusion
+    	'4184c143-98bd-4ca3-bfca-d40ed34acc9b' : ['map', ['germany']], // Were Germans Unified?
+    	'17387c82-756d-4571-b611-3380f33eeee4' : ['map', ['germany', 'denmark', 'france', 'poland', 'austria']], // What Does It Mean?!
         // --------------------
 
         // --------------------
     	// The decline of the ootoman empire 1878-1914
-    	'129dd05b-23a3-4ac9-934d-e46ccd96e736' : ['map'], // 'Treaty of San Stefano'
-    	'6c7d33d6-7f10-4c22-93fc-b1edde8d49b9' : ['gallery'], // 'Relations with Germany'
-    	'cbeb1ebb-8d3f-4c5b-9736-81197b017b57' : ['map'], //  'Balkan Wars'
-    	'0ccb5724-f943-4f6f-bfca-6d7817a161e6' : ['gallery'],// Italo-Libyan
-    	'0c496019-9c3f-4395-b1cc-9260daad6272' : ['gallery'], // The Goeben and Breslau Incident
-    	'3a71f13e-0bbb-4b34-8bd7-b4e27e61c40c' : ['map'], // General Decline of the Empire
-    	'7d6ae126-119e-485e-b668-073147249222' : ['gallery'],// title: Young Turks - 1906, id: 7d6ae126-119e-485e-b668-073147249222
-    	'9a0ad0c3-87c7-4c79-810c-104c88c55992' : ['map'], // title: The Beginning of the End, id: 9a0ad0c3-87c7-4c79-810c-104c88c55992
+    	'129dd05b-23a3-4ac9-934d-e46ccd96e736' : ['map', ['russia', 'albania', 'bosnia', 'bulgaria', 'croatia', 'greece', 'kosovo', 'macedonia', 'montenegro', 'romania', 'serbia', 'turkey']], // 'Treaty of San Stefano'
+    	'6c7d33d6-7f10-4c22-93fc-b1edde8d49b9' : ['gallery', ['germany', 'turkey', 'uk', 'france', 'turkey']], // 'Relations with Germany'
+    	'cbeb1ebb-8d3f-4c5b-9736-81197b017b57' : ['map', ['albania', 'bosnia', 'bulgaria', 'croatia', 'greece', 'kosovo', 'macedonia', 'montenegro', 'romania', 'serbia', 'turkey', 'austria', 'hungary', 'russia', 'italy']], //  'Balkan Wars'
+    	'0ccb5724-f943-4f6f-bfca-6d7817a161e6' : ['gallery', ['italy', 'turkey', 'libya']],// Italo-Libyan
+    	'0c496019-9c3f-4395-b1cc-9260daad6272' : ['gallery', ['germany', 'turkey', 'uk']], // The Goeben and Breslau Incident
+    	'3a71f13e-0bbb-4b34-8bd7-b4e27e61c40c' : ['map', ['turkey']], // General Decline of the Empire
+    	'7d6ae126-119e-485e-b668-073147249222' : ['gallery', ['turkey']],// title: Young Turks - 1906, id: 7d6ae126-119e-485e-b668-073147249222
+    	'9a0ad0c3-87c7-4c79-810c-104c88c55992' : ['map', ['albania', 'bosnia', 'bulgaria', 'croatia', 'greece', 'kosovo', 'macedonia', 'montenegro', 'romania', 'serbia', 'turkey']], // title: The Beginning of the End, id: 9a0ad0c3-87c7-4c79-810c-104c88c55992
         // --------------------
 
         // --------------------
         // alliances 1879 - 1914
-        '1d3ea481-5437-41ea-ab3c-c11f49e4756a' : ['gallery'], //title: Friends or Foes?, id: 1d3ea481-5437-41ea-ab3c-c11f49e4756a
-        'c452db2c-e212-4bd3-a59c-3bacb374ab54' : ['gallery'], // title: Triple Alliance (1882) , id: c452db2c-e212-4bd3-a59c-3bacb374ab54
-        '1a1bbe11-1709-4407-9653-cf8b98bd98f7' : ['gallery'],// title: Anglo-Japanese Alliance (1902), id: 1a1bbe11-1709-4407-9653-cf8b98bd98f7
-        'd1e02b06-5591-482a-92f3-a73075864aa5' : ['map'],// title: Alliances of World War One, id: d1e02b06-5591-482a-92f3-a73075864aa5
-        '1dec7e16-083b-4884-a81e-3a618ef440a5' : ['gallery'], // title: Dual Alliance (1879), id: 1dec7e16-083b-4884-a81e-3a618ef440a5
-        'e471a51c-c249-4876-9bc3-17c0e2a7ff9e' : ['gallery'], //title: The Franco-Russian Alliance Military Convention - August 18, 1892, id: e471a51c-c249-4876-9bc3-17c0e2a7ff9e
-        'b1643d22-373f-48f5-9ab6-34f99809bb28' : ['gallery'], // title: The Anglo-Russian Entente (1907), id: b1643d22-373f-48f5-9ab6-34f99809bb28
-        '182b388b-0ecf-4528-a935-ce7f85f8bb2c' : ['gallery'],// title: Anglo-French Entente (1904) , id: 182b388b-0ecf-4528-a935-ce7f85f8bb2c
+        '1d3ea481-5437-41ea-ab3c-c11f49e4756a' : ['gallery', ['italy', 'austria', 'germany', 'turkey', 'bulgaria', 'serbia', 'russia', 'france', 'britain']], //title: Friends or Foes?, id: 1d3ea481-5437-41ea-ab3c-c11f49e4756a
+        'c452db2c-e212-4bd3-a59c-3bacb374ab54' : ['gallery', ['germany', 'austria', 'hungary', 'czech', 'slovakia', 'france', 'italy', 'uk']], // title: Triple Alliance (1882) , id: c452db2c-e212-4bd3-a59c-3bacb374ab54
+        '1a1bbe11-1709-4407-9653-cf8b98bd98f7' : ['gallery', ['uk', 'china', 'japan']],// title: Anglo-Japanese Alliance (1902), id: 1a1bbe11-1709-4407-9653-cf8b98bd98f7
+        'd1e02b06-5591-482a-92f3-a73075864aa5' : ['map', ['japan', 'uk', 'austria', 'hungary', 'czech', 'slovakia', 'france', 'germany', 'italy', 'russia']],// title: Alliances of World War One, id: d1e02b06-5591-482a-92f3-a73075864aa5
+        '1dec7e16-083b-4884-a81e-3a618ef440a5' : ['gallery', ['austria', 'hungary', 'czech', 'slovakia', 'germany', 'italy']], // title: Dual Alliance (1879), id: 1dec7e16-083b-4884-a81e-3a618ef440a5
+        'e471a51c-c249-4876-9bc3-17c0e2a7ff9e' : ['gallery', ['france', 'russia']], //title: The Franco-Russian Alliance Military Convention - August 18, 1892, id: e471a51c-c249-4876-9bc3-17c0e2a7ff9e
+        'b1643d22-373f-48f5-9ab6-34f99809bb28' : ['gallery', ['france', 'uk', 'russia']], // title: The Anglo-Russian Entente (1907), id: b1643d22-373f-48f5-9ab6-34f99809bb28
+        '182b388b-0ecf-4528-a935-ce7f85f8bb2c' : ['gallery', ['france', 'uk']],// title: Anglo-French Entente (1904) , id: 182b388b-0ecf-4528-a935-ce7f85f8bb2c
         // --------------------
 
         // --------------------
         // War in Africa
-        '49c5e6c2-a6fc-49b5-9469-ee768dcb7a55' : ['gallery'], //title: War in Africa, id: 49c5e6c2-a6fc-49b5-9469-ee768dcb7a55
+        '49c5e6c2-a6fc-49b5-9469-ee768dcb7a55' : ['gallery', ['uk', 'netherlands', 'uk']], //title: War in Africa, id: 49c5e6c2-a6fc-49b5-9469-ee768dcb7a55
         // --------------------
 
         // --------------------
         // The Russo-Japanese War
-        '7ea8a182-7a35-4493-b1f6-f533d32a851a' : ['gallery'], //    title: Outbreak of the War, id: 7ea8a182-7a35-4493-b1f6-f533d32a851a
-        'dbcf2bee-99da-4525-9012-11b621abb470' : ['gallery'], //    title: A Shift of Power, id: dbcf2bee-99da-4525-9012-11b621abb470
-        'da4284a9-0207-4636-b0b8-9251c57c7b2f' : ['gallery'], //    title: European Impacts, id: da4284a9-0207-4636-b0b8-9251c57c7b2f
-        '2fe8922f-b04a-4f2c-bd45-0442a58c6b72' : ['gallery'], //    title: The Growth of Japan, id: 2fe8922f-b04a-4f2c-bd45-0442a58c6b72
-        'e7900349-364c-4177-abf0-cb1589a58342' : ['gallery'], //    title: Treaty of Portsmouth, id: e7900349-364c-4177-abf0-cb1589a58342 exhibitCollection.js:26
-        '024128ec-d1d8-4382-9d1d-e0223720e68d' : ['gallery'], //    title: Russia Defeated, id: 024128ec-d1d8-4382-9d1d-e0223720e68d
-        'a1985468-e206-4048-8e1b-e696d7cb1ea4' : ['gallery'], //    title: Impacts, id: a1985468-e206-4048-8e1b-e696d7cb1ea4
+        '7ea8a182-7a35-4493-b1f6-f533d32a851a' : ['gallery', ['russia', 'japan']], //    title: Outbreak of the War, id: 7ea8a182-7a35-4493-b1f6-f533d32a851a
+        'dbcf2bee-99da-4525-9012-11b621abb470' : ['gallery', ['russia', 'japan', 'germany', 'austria', 'hungary', 'czech', 'slovakia']], //    title: A Shift of Power, id: dbcf2bee-99da-4525-9012-11b621abb470
+        'da4284a9-0207-4636-b0b8-9251c57c7b2f' : ['gallery', ['russia', 'japan', 'uk']], //    title: European Impacts, id: da4284a9-0207-4636-b0b8-9251c57c7b2f
+        '2fe8922f-b04a-4f2c-bd45-0442a58c6b72' : ['gallery', ['russia', 'japan', 'uk', 'china']], //    title: The Growth of Japan, id: 2fe8922f-b04a-4f2c-bd45-0442a58c6b72
+        'e7900349-364c-4177-abf0-cb1589a58342' : ['gallery', ['russia', 'japan']], //    title: Treaty of Portsmouth, id: e7900349-364c-4177-abf0-cb1589a58342 exhibitCollection.js:26
+        '024128ec-d1d8-4382-9d1d-e0223720e68d' : ['gallery', ['russia', 'japan']], //    title: Russia Defeated, id: 024128ec-d1d8-4382-9d1d-e0223720e68d
+        'a1985468-e206-4048-8e1b-e696d7cb1ea4' : ['gallery', ['russia', 'france', 'uk']], //    title: Impacts, id: a1985468-e206-4048-8e1b-e696d7cb1ea4
         // --------------------
 
         // --------------------
         // The schlieffen Plan
-        'f301bbf4-93f2-4612-9296-afc0bef1d778' : ['gallery'], // title: General Count Alfred von Schlieffen, id: f301bbf4-93f2-4612-9296-afc0bef1d778
-        '96b37eff-a5b4-40ff-9155-214747e40475' : ['map'],     // title: Russia Rearming, id: 96b37eff-a5b4-40ff-9155-214747e40475
-        'be400897-e849-47e2-971b-854300355620' : ['gallery'], // title: The Alfred Redl Scandal, id: be400897-e849-47e2-971b-854300355620
-        'aae5717a-af2c-440e-bb67-477c5224457f' : ['gallery'], // title: Belgian Forts, id: aae5717a-af2c-440e-bb67-477c5224457f
-        '53d4abcb-34bf-4a2f-95e9-653ef835afae' : ['gallery'], // title: France and its Forts, id: 53d4abcb-34bf-4a2f-95e9-653ef835afae
-        '5ff91ed0-6cbf-454f-89a6-aacd5eca7ce6' : ['gallery'], // title: Belgian Neutrality, id: 5ff91ed0-6cbf-454f-89a6-aacd5eca7ce6
-        'aec3c8a6-4cde-4b2b-a9f5-aab3139326c4' : ['map'],     // title: Germany “Surrounded”, id: aec3c8a6-4cde-4b2b-a9f5-aab3139326c4
-        'c1a58fc9-1acd-4846-9211-098385424ad9' : ['gallery'], // title: Details, details, details..., id: c1a58fc9-1acd-4846-9211-098385424ad9
+        'f301bbf4-93f2-4612-9296-afc0bef1d778' : ['gallery', ['germany']], // title: General Count Alfred von Schlieffen, id: f301bbf4-93f2-4612-9296-afc0bef1d778
+        '96b37eff-a5b4-40ff-9155-214747e40475' : ['map', ['germany', 'russia', 'japan']],     // title: Russia Rearming, id: 96b37eff-a5b4-40ff-9155-214747e40475
+        'be400897-e849-47e2-971b-854300355620' : ['gallery', ['germany', 'russia', 'austria', 'hungary', 'czech', 'slovakia']], // title: The Alfred Redl Scandal, id: be400897-e849-47e2-971b-854300355620
+        'aae5717a-af2c-440e-bb67-477c5224457f' : ['gallery', ['germany', 'belgium', 'france']], // title: Belgian Forts, id: aae5717a-af2c-440e-bb67-477c5224457f
+        '53d4abcb-34bf-4a2f-95e9-653ef835afae' : ['gallery', ['germany', 'belgium', 'france']], // title: France and its Forts, id: 53d4abcb-34bf-4a2f-95e9-653ef835afae
+        '5ff91ed0-6cbf-454f-89a6-aacd5eca7ce6' : ['gallery', ['germany', 'belgium', 'uk']], // title: Belgian Neutrality, id: 5ff91ed0-6cbf-454f-89a6-aacd5eca7ce6
+        'aec3c8a6-4cde-4b2b-a9f5-aab3139326c4' : ['map', ['germany', 'uk']],     // title: Germany “Surrounded”, id: aec3c8a6-4cde-4b2b-a9f5-aab3139326c4
+        'c1a58fc9-1acd-4846-9211-098385424ad9' : ['gallery', ['germany', 'russia']], // title: Details, details, details..., id: c1a58fc9-1acd-4846-9211-098385424ad9
         // --------------------
 
         // --------------------
         // 1st Moroccan Crisis
-        '6db6a20b-8d3d-45e1-8f2b-4556f353b0f4' : ['gallery'], //title: The Algeciras Conference, id: 6db6a20b-8d3d-45e1-8f2b-4556f353b0f4
-        'c46acdad-fb74-4ed1-b21b-e462786f3bc4' : ['gallery'],//title: How the 1st Crisis Broke Out, id: c46acdad-fb74-4ed1-b21b-e462786f3bc4 exhibitCollection.js:26
-        '806d4379-57e2-44e7-8c56-6e881b5e5cca' : ['gallery'],//title: Something to Think About..., id: 806d4379-57e2-44e7-8c56-6e881b5e5cca
-        '4ef86390-1296-4613-957e-e6d45ad17e80' : ['gallery'],//title: Fashoda, id: 4ef86390-1296-4613-957e-e6d45ad17e80
+        '6db6a20b-8d3d-45e1-8f2b-4556f353b0f4' : ['gallery', ['morocco', 'germany', 'france']], //title: The Algeciras Conference, id: 6db6a20b-8d3d-45e1-8f2b-4556f353b0f4
+        'c46acdad-fb74-4ed1-b21b-e462786f3bc4' : ['gallery', ['morocco', 'germany', 'france']],//title: How the 1st Crisis Broke Out, id: c46acdad-fb74-4ed1-b21b-e462786f3bc4 exhibitCollection.js:26
+        '806d4379-57e2-44e7-8c56-6e881b5e5cca' : ['gallery', ['morocco', 'germany']],//title: Something to Think About..., id: 806d4379-57e2-44e7-8c56-6e881b5e5cca
+        '4ef86390-1296-4613-957e-e6d45ad17e80' : ['gallery', ['sudan', 'uk', 'france']],//title: Fashoda, id: 4ef86390-1296-4613-957e-e6d45ad17e80
 
         // --------------------
         // 2nd Moroccan Crisis
